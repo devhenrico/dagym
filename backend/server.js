6f1895ae8f3396bd25b9ee8bc21120e590ea0c37
Henrico Santos <henricosantos27@outlook.com>
2025-08-16 18:51:42 -0300

    update code commit

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ import { router } from "./routes.js";
 import cors from "cors";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(cors());
 
 app.use("/", router());
 
-app.listen(3000, () => console.log("Server running"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
